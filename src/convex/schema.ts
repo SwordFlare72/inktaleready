38bb28d165c7a5c08c50168dfec5dd108f780b19
vly.ai <support@vly.ai>
2025-10-02 15:19:46 +0000

    Revert to "Fix: Enhance username setting reliability during account creation"

--- src/convex/schema.ts
+++ src/convex/schema.ts
@@ -53,7 +53,7 @@ const schema = defineSchema(
     // the users table is the default users table that is brought in by the authTables
     users: defineTable({
       name: v.optional(v.string()), // name of the user. do not remove
-      image: v.optional(v.id("_storage")), // avatar image stored as storage ID
+      image: v.optional(v.string()), // image of the user. do not remove
       email: v.optional(v.string()), // email of the user. do not remove
       emailVerificationTime: v.optional(v.number()), // email verification time. do not remove
       isAnonymous: v.optional(v.boolean()), // is the user anonymous. do not remove
